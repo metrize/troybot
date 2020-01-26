dac7a45933a1ad8d86144c34385f13d36aad0ae4
TroyDota <troybensonsa@gmail.com>
2020-01-26 08:06:02 +0200

    Update songrequest.py

--- pajbot/models/songrequest.py
+++ pajbot/models/songrequest.py
@@ -61,7 +61,7 @@ class SongrequestQueue(Base):
         all_songs_before_current = (
             db_session.query(SongrequestQueue)
             .filter(SongrequestQueue.queue < self.queue)
-            .filter_by(requested_by_id=None)
+            .filter(SongrequestQueue.requested_by_id is None)
             .all()
         )
         time = 0

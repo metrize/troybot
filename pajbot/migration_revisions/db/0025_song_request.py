89b99a49426cad0da41c4b0ef63bd07eb37ed710
TroyDota <troybensonsa@gmail.com>
2020-02-24 18:46:12 +0200

    Update 0025_song_request.py

--- pajbot/migration_revisions/db/0025_song_request.py
+++ pajbot/migration_revisions/db/0025_song_request.py
@@ -7,5 +7,5 @@ def up(cursor, bot):
     cursor.execute("ALTER TABLE songrequest_queue DROP COLUMN queue;")
     cursor.execute("ALTER TABLE songrequest_queue DROP COLUMN playing;")
     cursor.execute("ALTER TABLE songrequest_queue DROP COLUMN current_song_time;")
-    cursor.execute("ALTER TABLE songrequest_queue ADD COLUMN date_resumed timestamp with time zone NOT NULL;")
+    cursor.execute("ALTER TABLE songrequest_queue ADD COLUMN date_resumed timestamp with time zone;")
     cursor.execute("ALTER TABLE played_for ADD COLUMN REAL DEFAULT 0 NOT NULL;")

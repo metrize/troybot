2ea9258399332f95d99108d98b35c0fe9711ccf6
TroyDota <troybensonsa@gmail.com>
2020-02-24 22:34:11 +0200

    Update songrequest_queue_manager.py

--- pajbot/managers/songrequest_queue_manager.py
+++ pajbot/managers/songrequest_queue_manager.py
@@ -11,17 +11,12 @@ class SongRequestQueueManager:
     streamer_name = None
     redis = None
     song_playing_id = None
-    song_queues = {}
 
     @staticmethod
     def init(streamer_name):
         SongRequestQueueManager.streamer_name = streamer_name
         SongRequestQueueManager.redis = RedisManager.get()
         SongRequestQueueManager.song_playing_id = SongRequestQueueManager.redis.get(f"{SongRequestQueueManager.streamer_name}:song-playing-id")
-        SongRequestQueueManager.song_queues = {
-            "song-queue": SongRequestQueueManager._get_init_redis("song-queue"),
-            "backup-song-queue": SongRequestQueueManager._get_init_redis("backup-song-queue")
-        }
 
     @staticmethod
     def update_song_playing_id(song_playing_id):
@@ -30,7 +25,7 @@ class SongRequestQueueManager:
 
     @staticmethod
     def inset_song(_id, queue, index=None):
-        song_queue = SongRequestQueueManager.song_queues.get(queue, None)
+        song_queue = SongRequestQueueManager._get_init_redis(queue)
         if song_queue is None:
             log.error(f"invalid queue {queue}")
             return False
@@ -49,7 +44,7 @@ class SongRequestQueueManager:
 
     @staticmethod
     def move_song(from_index, to_index, queue):
-        song_queue = SongRequestQueueManager.song_queues.get(queue, None)
+        song_queue = SongRequestQueueManager._get_init_redis(queue)
         if song_queue is None:
             log.error(f"invalid queue {queue}")
             return False
@@ -67,7 +62,7 @@ class SongRequestQueueManager:
 
     @staticmethod
     def remove_song(index, queue):
-        song_queue = SongRequestQueueManager.song_queues.get(queue, None)
+        song_queue = SongRequestQueueManager._get_init_redis(queue)
         if song_queue is None:
             log.error(f"invalid queue {queue}")
             return False
@@ -81,8 +76,8 @@ class SongRequestQueueManager:
 
     @staticmethod
     def remove_song_id(_id):
-        song_queue = SongRequestQueueManager.song_queues.get("song-queue", None)
-        backup_song_queue = SongRequestQueueManager.song_queues.get("backup-song-queue", None)
+        song_queue = SongRequestQueueManager._get_init_redis("song-queue")
+        backup_song_queue = SongRequestQueueManager._get_init_redis("backup-song-queue")
 
         if _id in song_queue:
             song_queue.remove(_id)
@@ -98,7 +93,7 @@ class SongRequestQueueManager:
 
     @staticmethod
     def get_id_index(_id):
-        song_queue = SongRequestQueueManager.song_queues.get("song-queue", None) + SongRequestQueueManager.song_queues.get("backup-song-queue", None)
+        song_queue = SongRequestQueueManager._get_init_redis("song-queue") + SongRequestQueueManager._get_init_redis("backup-song-queue")
 
         if _id not in song_queue:
             return -1
@@ -117,7 +112,7 @@ class SongRequestQueueManager:
 
     @staticmethod
     def _update_redis(queue):
-        song_queue = SongRequestQueueManager.song_queues.get(queue, None)
+        song_queue = SongRequestQueueManager._get_init_redis(queue)
         if song_queue is None:
             log.error(f"invalid queue {queue}")
             return
@@ -125,7 +120,7 @@ class SongRequestQueueManager:
 
     @staticmethod
     def _songs_before(_id, queue):
-        song_queue = SongRequestQueueManager.song_queues.get(queue, None)
+        song_queue = SongRequestQueueManager._get_init_redis(queue)
         if song_queue is None:
             log.error(f"invalid queue {queue}")
             return False
@@ -137,7 +132,7 @@ class SongRequestQueueManager:
 
     @staticmethod
     def _get_id(index, queue):
-        song_queue = SongRequestQueueManager.song_queues.get(queue, None)
+        song_queue = SongRequestQueueManager._get_init_redis(queue)
         if song_queue is None:
             log.error(f"invalid queue {queue}")
             return False
@@ -149,14 +144,14 @@ class SongRequestQueueManager:
 
     @staticmethod
     def get_next_song():
-        song_queue = SongRequestQueueManager.song_queues.get("song-queue", None)
-        backup_song_queue = SongRequestQueueManager.song_queues.get("backup-song-queue", None)
+        song_queue = SongRequestQueueManager._get_init_redis("song-queue")
+        backup_song_queue = SongRequestQueueManager._get_init_redis("backup-song-queue")
         return song_queue[0] if len(song_queue) != 0 else (backup_song_queue[0] if len(backup_song_queue) != 0 else None)
 
     @staticmethod
     def get_next_songs(limit):
-        song_queue = SongRequestQueueManager.song_queues.get("song-queue", None)
-        backup_song_queue = SongRequestQueueManager.song_queues.get("backup-song-queue", None)
+        song_queue = SongRequestQueueManager._get_init_redis("song-queue")
+        backup_song_queue = SongRequestQueueManager._get_init_redis"backup-song-queue")
         if not limit:
             return song_queue + backup_song_queue
 
@@ -173,4 +168,3 @@ class SongRequestQueueManager:
     @staticmethod
     def delete_backup_songs():
         SongRequestQueueManager.redis.set(f"{SongRequestQueueManager.streamer_name}:backup-song-queue", "[]")
-        SongRequestQueueManager.song_queues["backup-song-queue"] = []
